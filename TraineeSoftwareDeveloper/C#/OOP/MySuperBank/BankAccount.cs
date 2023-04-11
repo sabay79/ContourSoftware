741b34eec722d4b12465c59809fff7b03e5379f9
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-04-11 12:02:08 +0500

    C#: Object Oriented Programming - Methods and Members
    https://learn.microsoft.com/en-us/shows/csharp-101/csharp-object-oriented-programming-methods-and-members

--- TraineeSoftwareDeveloper/C#/OOP/MySuperBank/BankAccount.cs
+++ TraineeSoftwareDeveloper/C#/OOP/MySuperBank/BankAccount.cs
@@ -10,9 +10,27 @@ namespace MySuperBank
     {
         public string Number { get; }
         public string Owner { get; }
-        public decimal Balance { get; }
+        public decimal Balance 
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance = +item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
         public BankAccount(string name, decimal initialBalance)
         {
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
             this.Owner = name;
             this.Balance = initialBalance;
         }
