a5e4d3bd85e99c0d92fe8d894486a5461e96a928
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-04-11 11:47:12 +0500

    C#: Object Oriented Programming - Objects and Classes
    https://learn.microsoft.com/en-us/shows/csharp-101/csharp-object-oriented-programming-objects-and-classes

---
+++ TraineeSoftwareDeveloper/C#/OOP/MySuperBank/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; }
+        public decimal Balance { get; }
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+        }
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+    }
+}
---
+++ TraineeSoftwareDeveloper/C#/OOP/MySuperBank/Program.cs
@@ -0,0 +1,6 @@
+﻿using MySuperBank;
+
+Console.WriteLine("---------- MY SUPER BANK ----------");
+var account = new BankAccount("Saba", 30000);
+// String Interpolation
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
