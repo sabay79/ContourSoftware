a0edfed4ebad227914d9e32a58213abb0e7f0d12
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-04-14 15:44:26 +0500

    C# Expressions
    String Interpolation
    Lambda Expression
    Query Expression

---
+++ TraineeSoftwareDeveloper/C#/1.Fundamentals/3.Expressions/Program.cs
@@ -0,0 +1,28 @@
+﻿// EXPRESSIONS //
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/expressions
+
+// 1. Interpolated String Expression
+// Provide convenient syntax to create formatted strings
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated
+var r = 2.3;
+var message = $"Area of Circle with Radius {r} is {Math.PI * r * r:F2}";
+Console.WriteLine(message);
+
+// 2. Lambda Expression
+// Allow you to create anonymous functions
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/lambda-expressions
+int[] num = { 2, 4, 6, 7, 10 };
+var maxSquare = num.Max(x => x * x);
+Console.WriteLine("Square of Greatest Number is " + maxSquare);
+
+// 3. Query Expression
+// Allow you to use query capabilities directly in C#
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/query-keywords
+var scores = new[] { 10, 20, 50, 90, 70, 80 };
+IEnumerable<int> highScoresQuery =
+    from score  in scores
+    where score > 50
+    orderby score descending
+    select score;
+Console.WriteLine("Highest Scores are" + string.Join(", ", highScoresQuery));
