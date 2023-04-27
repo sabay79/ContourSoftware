320b415954b9a898247b3faa687be8c81264113c
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-04-27 15:20:24 +0500

    Add a Pizza list page with a form - Completed
    https://learn.microsoft.com/en-us/training/modules/create-razor-pages-aspnet-core/5-exercise-create-new-razor-page-and-form

--- TraineeSoftwareDeveloper/ASP.NET/RazorPagesPizza/Pages/Pizza.cshtml.cs
+++ TraineeSoftwareDeveloper/ASP.NET/RazorPagesPizza/Pages/Pizza.cshtml.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using RazorPagesPizza.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPagesPizza.Pages
 {
     public class PizzaModel : PageModel
     {
+    [BindProperty]
+    public Pizza NewPizza { get; set; }        
+    public List<Pizza> pizzas = new List<Pizza>();
         public void OnGet()
         {
         }
