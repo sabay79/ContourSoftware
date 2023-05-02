32a7c9aeb1b7ea3d8a46a5f7cd9f1a225236b6bc
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-02 16:20:27 +0500

    int result = Fibonacci(5); Console.WriteLine(result);  static int Fibonacci(int n) {     int n1 = 0;     int n2 = 1;     int sum;      for (int i = 2; i <= n; i++)     {         sum = n1 + n2;         n1 = n2;         n2 = sum;     }      return n == 0 ? n1 : n2; }
    https://learn.microsoft.com/en-us/training/modules/dotnet-debug/4-use-visual-studio-code-debugger

--- TraineeSoftwareDeveloper/ASP.NET/DotNetDependencies/Program.cs
+++ TraineeSoftwareDeveloper/ASP.NET/DotNetDependencies/Program.cs
@@ -25,3 +25,26 @@ HumanizerQuantities();
 
 Console.WriteLine("\nDate/Time Manipulation:");
 HumanizerDates();
+
+
+// DotNet Debugging //
+
+//Exercise - Debug with Visual Studio Code
+int result = Fibonacci(5);
+Console.WriteLine(result);
+
+static int Fibonacci(int n)
+{
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    for (int i = 2; i <= n; i++)
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+    }
+
+    return n == 0 ? n1 : n2;
+}
