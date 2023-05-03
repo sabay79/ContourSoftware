93fdba7d37f6693a969cc8512fdf5933024d486a
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-03 17:38:08 +0500

    C#: Query Expression - LINQ
    https://learn.microsoft.com/en-us/dotnet/csharp/linq/query-expression-basics

--- TraineeSoftwareDeveloper/C#/3_LINQ/3_LINQ/LINQ/Program.cs
+++ TraineeSoftwareDeveloper/C#/3_LINQ/3_LINQ/LINQ/Program.cs
@@ -108,21 +108,88 @@ IEnumerable<int> numQuery1 = from num in numbers
 IEnumerable<int> numQuery2 = numbers.Where(num => num % 2 == 0)
     .OrderBy(num => num);
 
-
-
 // Execute the query
 foreach (var temp in numQuery2)
     Console.Write(temp + " ");
 
+// Query expression basics // 
+// https://learn.microsoft.com/en-us/dotnet/csharp/linq/query-expression-basics
 
+// A query expression is a query expressed in query syntax.
+// A query is a set of instructions that describes what data to retrieve from a given data source (or sources) and what shape and organization the returned data should have.
+// A query is distinct from the results that it produces.
+/*
+ * From an application's viewpoint, the specific type and structure of the original source data is not important. 
+ * The application always sees the source data as an IEnumerable<T> or IQueryable<T> collection. 
+ * For example, in LINQ to XML, the source data is made visible as an IEnumerable<XElement>.
+ * Given this source sequence, a query may do one of three things:
+ */
+
+/* 1.
+ * Retrieve a subset of the elements to produce a new sequence without modifying the individual elements. 
+ * The query may then sort or group the returned sequence in various ways, as shown in the following example (assume scores is an int[]):
+ */
+IEnumerable<int> highScoreQuery1 = from score in scores
+                                   where score > 70
+                                   orderby score descending
+                                   select score;
+/* 2.
+ * Retrieve a sequence of elements as in the previous example but transform them to a new type of object. 
+ * For example, a query may retrieve only the last names from certain customer records in a data source. 
+ * Or it may retrieve the complete record and then use it to construct another in-memory object type or even XML data before generating the final result sequence. 
+ * The following example shows a projection from an int to a string. 
+ * Note the new type of highScoresQuery.
+ */
+IEnumerable<string> highScoreQuery2 = from score in scores
+                                      where score > 70
+                                      orderby score descending
+                                      select $"The score is {score}";
+/* 3.
+ * Retrieve a singleton value about the source data, such as:
+- The number of elements that match a certain condition.
+- The element that has the greatest or least value.
+- The first element that matches a condition, or the sum of particular values in a specified set of elements. 
+For example, the following query returns the number of scores greater than 80 from the scores integer array:
+ */
+int highScoreCount = (from score in scores
+                      where score > 80
+                      select score).Count();
+// OR - You can also express this by using a new variable to store the concrete result.
+// This technique is more readable because it keeps the variable that stores the query separate from the query that stores a result.
+IEnumerable<int> highScoreQuery3 = from score in scores
+                                   where score > 80
+                                   select score;
+int highScoreCount1 = highScoreQuery3.Count();
 /*
  Query expressions can be compiled to expression trees or to delegates, depending on the type that the query is applied to. 
  IEnumerable<T> queries are compiled to delegates. 
  IQueryable and IQueryable<T> queries are compiled to expression trees.
  */
 
+// Query Variable //
+// In above example, highScoreQuery1 is query variable but highScoreQuery3 is not because highScoreQuery3 store results
+// In LINQ, a query variable is any variable that stores a query instead of the results of a query. 
+// if use any operation which result in storgae of some value, that won't be a query Variable anymore - Count, Max, Min, Sum, ToList .etc
 
+// Explicit and Implicit typing of query variables //
+// Use direct type like IEnumerable<T> 
+// Implicit - Use var instead of IEnumerable<T> 
+// -> expression is compiled as IEnumerable<Customer> or perhaps IQueryable<Customer>
 
+// Starting a query expression //
+// A query expression must begin with a from clause.
+
+// Multiple From Clause
+var countries = CountrySevice.GetAll();
+var cityQuery = from country in countries
+                from city in country.Cities
+                select city;
+foreach (var city in cityQuery)
+    Console.WriteLine(city);
+// Debug
+
+// Ending a query expression //
+// A query expression must end with either a group clause or a select clause.
 public class Person
 {
     public int ID { get; set; }
@@ -145,7 +212,35 @@ public static class PersonService
     public static List<Person> GetAll() => People;
     public static void Add(Person person)
     {
+        person.ID = nextID;
         People.Add(person);
+        nextID++;
     }
 }
 
+public class Country
+{
+    public int ID { get; set; }
+    public string? Name { get; set; }
+    public List<string>? Cities { get; set; }
+}
+public static class CountrySevice
+{
+    public static List<Country> Countries { get; set; }
+    static int nextID = 2;
+    static CountrySevice()
+    {
+        Countries = new List<Country>()
+        {
+            new Country { ID = 1, Name = "Pakistan", Cities = new List<string> { "Islamabad", "Lahore", "Karachi" } }
+        };
+    }
+    public static List<Country> GetAll() => Countries;
+    public static void Add(Country country)
+    {
+        country.ID = nextID;
+        Countries.Add(country);
+        nextID++;
+    }
+
+}
