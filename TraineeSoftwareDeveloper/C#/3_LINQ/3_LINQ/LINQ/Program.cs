e97c34b143a774727e98b81e038d530caf91eed5
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-03 09:50:26 +0500

    What is LINQ?
    https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/

--- TraineeSoftwareDeveloper/C#/3_LINQ/3_LINQ/LINQ/Program.cs
+++ TraineeSoftwareDeveloper/C#/3_LINQ/3_LINQ/LINQ/Program.cs
@@ -1,1 +1,8 @@
 ﻿// LINQ - Language Integrated Query //
+
+// Language-Integrated Query (LINQ) is the name for a set of technologies based on the integration of query capabilities directly into the C# language.
+// Traditionally, queries against data are expressed as simple strings without type checking at compile time or IntelliSense support. 
+// Furthermore, you have to learn a different query language for each type of data source: SQL databases, XML documents, various Web services, and so on. 
+// With LINQ, a query is a first-class language construct, just like classes, methods, events. You write queries against strongly typed collections of objects by using language keywords and familiar operators. 
+// he LINQ family of technologies provides a consistent query experience for objects (LINQ to Objects), relational databases (LINQ to SQL), and XML (LINQ to XML).
+
