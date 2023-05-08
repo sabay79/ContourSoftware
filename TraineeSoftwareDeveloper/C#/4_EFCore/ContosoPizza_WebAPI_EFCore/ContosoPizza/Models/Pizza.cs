a56e37543cc75a4c3423a544af667f44c636581f
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-08 10:41:08 +0500

    Change the model and update the database schema
    https://learn.microsoft.com/en-us/training/modules/persist-data-ef-core/3-migrations

--- TraineeSoftwareDeveloper/C#/4_EFCore/ContosoPizza_WebAPI_EFCore/ContosoPizza/Models/Pizza.cs
+++ TraineeSoftwareDeveloper/C#/4_EFCore/ContosoPizza_WebAPI_EFCore/ContosoPizza/Models/Pizza.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContosoPizza.Models;
 
 public class Pizza
 {
+    // Id or <entity name>Id are inferred by EF Core to be the primary key
     public int Id { get; set; }
 
+    [Required]
+    [MaxLength(50)]
     public string? Name { get; set; }
 
     public Sauce? Sauce { get; set; }
--- TraineeSoftwareDeveloper/C#/4_EFCore/ContosoPizza_WebAPI_EFCore/ContosoPizza/Models/Topping.cs
+++ TraineeSoftwareDeveloper/C#/4_EFCore/ContosoPizza_WebAPI_EFCore/ContosoPizza/Models/Topping.cs
@@ -1,7 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace ContosoPizza.Models;
 
 public class Topping
 {
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
     public string? Name { get; set; }
+    public decimal Calories { get; set; }
+
+    [JsonIgnore]
+    // To prevent Topping entities from including the Pizzas property when the web API code serializes the response to JSON
+    public ICollection<Pizza>? Pizzas { get; set; }
 }
