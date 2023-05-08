3797fd125b33169efab35ca7c72c6848fe1eb02c
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-08 10:04:29 +0500

    Clone Repo & Added NuGet Packages
    git clone https://github.com/MicrosoftDocs/mslearn-persist-data-ef-core
    
    dotnet add package Microsoft.EntityFrameworkCore.Sqlite
    
    dotnet add package Microsoft.EntityFrameworkCore.Design
    
    dotnet tool install --global dotnet-ef

---
+++ TraineeSoftwareDeveloper/C#/4_EFCore/ContosoPizza_WebAPI_EFCore/ContosoPizza/Program.cs
@@ -0,0 +1,33 @@
+using ContosoPizza.Services;
+// Additional using declarations
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add the PizzaContext
+
+// Add the PromotionsContext
+
+builder.Services.AddScoped<PizzaService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Add the CreateDbIfNotExists method call
+
+app.Run();
