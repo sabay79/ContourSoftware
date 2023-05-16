2a789ceabddbfc949c8c436a75919cfe9e2dc6b5
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-16 10:54:55 +0500

    Initialize DB with test data - Seed the Database
    https://learn.microsoft.com/en-us/aspnet/mvc/overview/getting-started/getting-started-with-ef-using-mvc/creating-an-entity-framework-data-model-for-an-asp-net-mvc-application#initialize-db-with-test-data
    
    https://learn.microsoft.com/en-us/aspnet/core/data/ef-rp/intro?view=aspnetcore-7.0&tabs=visual-studio#create-the-database

--- TraineeSoftwareDeveloper/C#/5_EFCore_RepositoryPattern_UnitOfWork/ContosoUniversity/Program.cs
+++ TraineeSoftwareDeveloper/C#/5_EFCore_RepositoryPattern_UnitOfWork/ContosoUniversity/Program.cs
@@ -1,3 +1,5 @@
+using ContosoUniversity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +13,14 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<UniversityDbContext>();
+    dbContext.Database.EnsureCreated();
+    UniversityDbInitializer.Initialize(dbContext);
 }
 
 app.UseHttpsRedirection();
