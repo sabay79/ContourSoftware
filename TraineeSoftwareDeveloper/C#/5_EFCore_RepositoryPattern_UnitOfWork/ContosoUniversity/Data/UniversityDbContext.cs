7c9f351f6fb9ea60c215de17786156b30ce2f4c4
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-16 10:26:38 +0500

    Create the Database Context
    https://learn.microsoft.com/en-us/aspnet/mvc/overview/getting-started/getting-started-with-ef-using-mvc/creating-an-entity-framework-data-model-for-an-asp-net-mvc-application#create-the-database-context

---
+++ TraineeSoftwareDeveloper/C#/5_EFCore_RepositoryPattern_UnitOfWork/ContosoUniversity/Data/UniversityDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+// using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.Data
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options) : base(options)
+        { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
