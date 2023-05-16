ba47f22c65e335a5694b76480e135da5b2ea88e0
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-16 16:00:55 +0500

    Create an About page
    https://learn.microsoft.com/en-us/aspnet/mvc/overview/getting-started/getting-started-with-ef-using-mvc/sorting-filtering-and-paging-with-the-entity-framework-in-an-asp-net-mvc-application#create-an-about-page

--- TraineeSoftwareDeveloper/C#/5_EFCore_RepositoryPattern_UnitOfWork/ContosoUniversity/Controllers/HomeController.cs
+++ TraineeSoftwareDeveloper/C#/5_EFCore_RepositoryPattern_UnitOfWork/ContosoUniversity/Controllers/HomeController.cs
@@ -1,21 +1,38 @@
-﻿using ContosoUniversity.Models;
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using ContosoUniversity.Data;
+using ContosoUniversity.ViewModels;
 
 namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private UniversityDbContext _dbContext;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(UniversityDbContext dbContext,ILogger<HomeController> logger)
         {
+            _dbContext = dbContext;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult About()
+        {
+            IQueryable<EnrollmentDateGroup> data = from student in _dbContext.Student
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            return View(data.ToList());
         }
 
         public IActionResult Privacy()
@@ -27,6 +44,12 @@ namespace ContosoUniversity.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _dbContext.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
