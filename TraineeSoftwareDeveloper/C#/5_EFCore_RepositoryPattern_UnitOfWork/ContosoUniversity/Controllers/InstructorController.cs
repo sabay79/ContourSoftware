c0ded82bdce5f914cf872c4ac8c5684f86604aaf
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-17 11:54:24 +0500

    Read related data with EF Core in an ASP.NET MVC app
    https://learn.microsoft.com/en-us/aspnet/mvc/overview/getting-started/getting-started-with-ef-using-mvc/reading-related-data-with-the-entity-framework-in-an-asp-net-mvc-application

---
+++ TraineeSoftwareDeveloper/C#/5_EFCore_RepositoryPattern_UnitOfWork/ContosoUniversity/Controllers/InstructorController.cs
@@ -0,0 +1,180 @@
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using ContosoUniversity.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Controllers
+{
+    public class InstructorController : Controller
+    {
+        private readonly UniversityDbContext _context;
+
+        public InstructorController(UniversityDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Instructor
+        public IActionResult Index(int? id, int? courseID)
+        {
+            var viewModel = new InstructorIndexData();
+            viewModel.Instructors = _context.Instructors
+                                    .Include(i => i.OfficeAssignment)
+                                    .Include(i => i.Courses.Select(c => c.Department))
+                                    .OrderBy(i => i.LastName);
+
+            if (id != null)
+            {
+                ViewBag.InstructorID = id.Value;
+                viewModel.Courses = viewModel.Instructors
+                                    .Where(i => i.ID == id)
+                                    .Single()
+                                    .Courses;
+            }
+
+            if (courseID != null)
+            {
+                ViewBag.CourseID = courseID.Value;
+                viewModel.Enrollments = viewModel.Courses
+                    .Where(c => c.CourseID == courseID)
+                    .Single()
+                    .Enrollments;
+            }
+            return View(viewModel);
+        }
+
+        // GET: Instructor/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Instructors == null)
+            {
+                return NotFound();
+            }
+
+            var instructor = await _context.Instructors
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+
+            return View(instructor);
+        }
+
+        // GET: Instructor/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Instructor/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,LastName,FirstMidName,HireDate")] Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(instructor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(instructor);
+        }
+
+        // GET: Instructor/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Instructors == null)
+            {
+                return NotFound();
+            }
+
+            var instructor = await _context.Instructors.FindAsync(id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+            return View(instructor);
+        }
+
+        // POST: Instructor/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,LastName,FirstMidName,HireDate")] Instructor instructor)
+        {
+            if (id != instructor.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(instructor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InstructorExists(instructor.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(instructor);
+        }
+
+        // GET: Instructor/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Instructors == null)
+            {
+                return NotFound();
+            }
+
+            var instructor = await _context.Instructors
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+
+            return View(instructor);
+        }
+
+        // POST: Instructor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Instructors == null)
+            {
+                return Problem("Entity set 'UniversityDbContext.Instructors'  is null.");
+            }
+            var instructor = await _context.Instructors.FindAsync(id);
+            if (instructor != null)
+            {
+                _context.Instructors.Remove(instructor);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InstructorExists(int id)
+        {
+            return (_context.Instructors?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
