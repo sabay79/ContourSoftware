76f5468fb90b5813be3734c108d01475cb752b7e
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-18 18:00:25 +0500

    Generic Repository Implementation

---
+++ TraineeSoftwareDeveloper/ASP.NET/OnlineBookStore/Repository/OBS.Data.Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace OBS.Data.Interfaces
+{
+    public interface IGenericRepository<TEntity>
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetByID(int id);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(int id);
+        void Save();
+    }
+}
---
+++ TraineeSoftwareDeveloper/ASP.NET/OnlineBookStore/Repository/OBS.Data.Services/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OBS.Data.Interfaces;
+
+namespace OBS.Data.Services
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly BookStoreDbContext _dbContext;
+        private readonly DbSet<TEntity> _dbSet;
+        public GenericRepository()
+        {
+            _dbContext = new BookStoreDbContext();
+            _dbSet = _dbContext.Set<TEntity>();
+        }
+
+        public IEnumerable<TEntity> GetAll() => _dbSet.ToList();
+        public TEntity GetByID(int id) => _dbSet.Find(id);
+        public void Add(TEntity entity) => _dbSet.Add(entity);
+        public void Update(TEntity entity) => _dbSet.Update(entity);
+        public void Delete(int id)
+        {
+            var entity = GetByID(id);
+            _dbSet.Remove(entity);
+        }
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+    }
+}
