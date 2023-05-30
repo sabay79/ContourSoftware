6dc1f788300a40eff7bf53a170930a54877e08e9
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-30 17:59:20 +0500

    C# Sealed Class

---
+++ TraineeSoftwareDeveloper/C#/7_ClassTypes/SealedClass/Program.cs
@@ -0,0 +1,26 @@
+﻿// SEALED CLASS // 
+// Prevent Inheritance - can't be Inherited so can't be a base class
+// Can be useful in scenarios where you want to prevent further modification or extension of a class to maintain stability, security, or to enforce certain design decisions.
+// Difference from static class: we can't create instance of static class but of sealed class 
+
+Console.WriteLine("Sealed Class Example");
+//DerivedClass dc = new DerivedClass();
+//dc.BaseMethod();
+
+BaseClass bc = new BaseClass();
+bc.BaseMethod();
+
+public sealed class BaseClass
+{
+    public void BaseMethod()
+    {
+        Console.WriteLine("Method of Base Class");
+    }
+}
+//public class DerivedClass : BaseClass
+//{
+//    public void DerivedMethod()
+//    {
+//        Console.WriteLine("Method of Base Class");
+//    }
+//}
