cac56045896eb579b33761e1e9751b3949993036
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-30 18:13:52 +0500

    C# Partial Class - Partially Implemented

---
+++ TraineeSoftwareDeveloper/C#/7_ClassTypes/PartialClass/Program.cs
@@ -0,0 +1,24 @@
+﻿// PARTIAL CLASS //
+// Provides ability to implement functionality of a single class into multiple classes/files
+// All these files are combined into a single class file when application is compiled
+// Partial keyword can also used with Structs & Interfaces
+
+Console.WriteLine("Partial Class Example");
+
+class Student
+{
+    private string _firtsName;
+    private string _lastName;
+
+    public string FirstName
+    {
+        set { _firtsName = value; }
+        get { return _firtsName; }
+    }
+
+    public string LastName 
+    { 
+        get => _lastName; 
+        set => _lastName = value; 
+    }
+}
