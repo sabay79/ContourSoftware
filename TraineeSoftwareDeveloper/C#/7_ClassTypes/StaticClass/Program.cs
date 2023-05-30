3b4bd184244924c1a2dc8bd08236b6adb8b5c53d
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-30 12:24:23 +0500

    C# Static Class

---
+++ TraineeSoftwareDeveloper/C#/7_ClassTypes/StaticClass/Program.cs
@@ -0,0 +1,32 @@
+﻿// STATIC CLASS //
+// Can only have static properties, members, constructor & methods
+// Cannot be Inherited
+// Can't be Instantiated (cannnot create object/instance of class using new keyword)
+
+
+Console.WriteLine("Static Class Example");
+Product.Display();
+
+public static class Product
+{
+    public static int ID;
+    public static string Name;
+    public static int Price;
+
+    private static int nextID = 1;
+
+    static Product()
+    {
+        ID = nextID;
+        Name = "Piano";
+        Price = 10000;
+        nextID++;
+    }
+
+    public static void Display()
+    {
+        Console.WriteLine($"Product ID = {ID} \n" +
+                          $"Name = {Name} \n" +
+                          $"Price = {Price}");
+    }
+}
