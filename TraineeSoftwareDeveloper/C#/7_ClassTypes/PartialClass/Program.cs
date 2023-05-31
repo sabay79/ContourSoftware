45dc8b88c1e63b6c7f761c11a3828f2a6f5ce1f6
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-05-31 13:02:42 +0500

    C# Sealed Method

--- TraineeSoftwareDeveloper/C#/7_ClassTypes/PartialClass/Program.cs
+++ TraineeSoftwareDeveloper/C#/7_ClassTypes/PartialClass/Program.cs
@@ -3,7 +3,7 @@
 // All these files are combined into a single class file when application is compiled
 // All parts must have same accessibility, such as public, private, and so on.
 // Partial keyword can also used with Structs & Interfaces
-// multiple developers can work on single class in seperate files 
+// Multiple developers can work on single class in seperate files 
 
 using PartialClass;
 
---
+++ TraineeSoftwareDeveloper/C#/8_MethodTypes/MethodTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿// SEALED METHOD // 
+// Sealed Method is always an override method of child class
+// Sealed Method can't be overrided
+// Sealed Method is only available with Method Overriding (override)
+// Sealed Method is not available with method hiding (new)
+// Normal method can't be sealed
+
+Console.WriteLine("Sealed Method Practice");
+C obj = new();
+obj.Print();
+
+class A
+{
+    public virtual void Print()
+    {
+        Console.WriteLine("Class A");
+    }
+}
+class B : A
+{
+    // To prevent overriding of overrided method
+    public sealed override void Print()
+    {
+        Console.WriteLine("Class B");
+    }
+}
+class C : B
+{
+    // Can't ovveride Print() from B bcz it's sealed now
+    public new void Print()
+    {
+        Console.WriteLine("Class C");
+    }
+}
