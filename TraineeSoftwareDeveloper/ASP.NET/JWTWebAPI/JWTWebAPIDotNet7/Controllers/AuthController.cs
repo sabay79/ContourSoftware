bd7305cab16aae83f6306efd2b036b8ce837e9a9
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-06-05 15:14:59 +0500

    05-06-2023

--- TraineeSoftwareDeveloper/ASP.NET/JWTWebAPI/JWTWebAPIDotNet7/Controllers/AuthController.cs
+++ TraineeSoftwareDeveloper/ASP.NET/JWTWebAPI/JWTWebAPIDotNet7/Controllers/AuthController.cs
@@ -65,7 +65,7 @@ namespace JWTWebAPIDotNet7.Controllers
 
             
             // Alternate, if key is not stored in AppSetting.json
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("AaBbCDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"));
+            //var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("AaBbCDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"));
 
             // Credentials are needed with the key and then signatures 
             var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
