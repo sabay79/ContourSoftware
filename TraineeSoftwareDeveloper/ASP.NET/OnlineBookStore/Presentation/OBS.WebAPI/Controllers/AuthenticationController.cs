b0d0738cb9021304b6cac9df431c855259989ae4
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-06-06 15:56:48 +0500

    Add Roles In Asp.Net Core Identity | Role Based Authorization in Asp.net Core Identity API | Part-3
    https://www.youtube.com/watch?v=fkhOPhYSkuU
    Modify code in Register (Check for Role in Database & Assign Role to user)

--- TraineeSoftwareDeveloper/ASP.NET/OnlineBookStore/Presentation/OBS.WebAPI/Controllers/AuthenticationController.cs
+++ TraineeSoftwareDeveloper/ASP.NET/OnlineBookStore/Presentation/OBS.WebAPI/Controllers/AuthenticationController.cs
@@ -9,10 +9,10 @@ namespace OBS.WebAPI.Controllers
     public class AuthenticationController : ControllerBase
     {
         private readonly UserManager<IdentityUser> _userManager;
-        private readonly RoleManager<IdentityUser> _roleManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IConfiguration _configuration;
         public AuthenticationController(UserManager<IdentityUser> userManager,
-               RoleManager<IdentityUser> roleManager, IConfiguration configuration)
+               RoleManager<IdentityRole> roleManager, IConfiguration configuration)
         {
             _userManager = userManager;
             _roleManager = roleManager;
@@ -41,13 +41,36 @@ namespace OBS.WebAPI.Controllers
                 SecurityStamp = Guid.NewGuid().ToString(),
                 UserName = registerUser.Username
             };
-
-            var result = await _userManager.CreateAsync(user, registerUser.Password);
-            return result.Succeeded
-                ? StatusCode(StatusCodes.Status201Created,
-                    new Response { Status = "Success", Message = "User Created Successfully!" })
-                : (IActionResult)StatusCode(StatusCodes.Status500InternalServerError,
-                    new Response { Status = "Error", Message = "Failed to Create User!" });
+            if (await _roleManager.RoleExistsAsync(role))
+            {
+                var result = await _userManager.CreateAsync(user);
+                if (!result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError,
+                        new Response
+                        {
+                            Status = "Error",
+                            Message = "Failed to Create User!"
+                        });
+                }
+                // Assign role to a user
+                await _userManager.AddToRoleAsync(user, role);
+                return StatusCode(StatusCodes.Status200OK,
+                    new Response
+                    {
+                        Status = "Success",
+                        Message = "User Created Successfully!"
+                    });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new Response
+                    {
+                        Status = "Error",
+                        Message = "Role Doesnot Exist!"
+                    });
+            }
         }
     }
 }
