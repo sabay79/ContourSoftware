29b7aff42af75080800954ca158945429779e22a
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-06-14 12:28:58 +0500

    C# Multiple Inheritance

---
+++ TraineeSoftwareDeveloper/C#/11_Inheritance/MultipleInheritance/Program.cs
@@ -0,0 +1,47 @@
+﻿// MULTIPLE INHERITANCE //
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Multiple Inheritance in C#");
+        CA cA = new();
+        cA.Check();
+
+        CB cB = new();
+        cB.Check();
+
+        CC cC = new();
+        cC.Check();
+    }
+}
+// Multilevel Inheritance
+// 1. Multiple Parents
+public class PA { }
+public class PB { }
+
+//public class PC : PA, PB { } // Error CS1721  Class 'PC' cannot have multiple base classes: 'PA' and 'PB' // User Interface in this case
+
+// 2. Multiple Childs
+public class CA
+{
+    public virtual void Check()
+    {
+        Console.WriteLine("Base");
+    }
+}
+public class CB : CA
+{
+    public override void Check()
+    {
+        Console.WriteLine("Derived 1");
+    }
+}
+public class CC : CA
+{
+    public override void Check()
+    {
+        base.Check();
+        Console.WriteLine("Derived 2");
+    }
+}
