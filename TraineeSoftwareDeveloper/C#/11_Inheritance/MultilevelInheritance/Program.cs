1622a53d502bfac977e54e5171f313a1d3454d96
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-06-14 12:36:06 +0500

    C# Multilevel Inheritance

---
+++ TraineeSoftwareDeveloper/C#/11_Inheritance/MultilevelInheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿// MULTILEVEL INHERITANCE //
+
+Console.WriteLine("Multilevel Inheritance");
+Console.WriteLine("-");
+A a = new();
+a.Check();
+
+Console.WriteLine("-");
+B b = new();
+b.Check();
+
+Console.WriteLine("-");
+C c = new();
+c.Check();
+
+Console.WriteLine("-");
+
+public class A
+{
+    public virtual void Check()
+    {
+        Console.WriteLine("A");
+    }
+}
+
+public class B : A
+{
+    public override void Check()
+    {
+        base.Check(); // Comment and Check
+        Console.WriteLine("B");
+    }
+}
+
+public class C : B
+{
+    public override void Check()
+    {
+        base.Check();
+        Console.WriteLine("C");
+    }
+}
