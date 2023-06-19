d0d235c006191ccf933e606d9b8bc9a5b9e2d800
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-06-19 13:01:36 +0500

    C# Convert vs Parse
    Parse takes only string as Input
    Convert can take any data type as an Input

---
+++ TraineeSoftwareDeveloper/C#/14_ConvertVsParse/ConvertVsParse/Program.cs
@@ -0,0 +1,68 @@
+﻿// CONVERT VS PARSE //
+
+try
+{
+    Console.Write("Enter a number: ");
+    int temp = int.Parse(Console.ReadLine());
+    Console.WriteLine(temp);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+try
+{
+    Console.Write("Enter a number: ");
+    int temp = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(temp);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+try
+{
+    int temp1 = Int32.Parse("1"); // Only take string as input
+    Console.WriteLine(temp1);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+try
+{
+    int temp2 = Convert.ToInt32(12); // any d-type as input
+    Console.WriteLine(temp2);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+try
+{
+    object temp3 = "123"; // Error: if other than string
+    int temp4 = int.Parse((string)temp3);
+    Console.WriteLine(temp4);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+try
+{
+    object temp3 = 1234;
+    int temp5 = Convert.ToInt32(temp3);
+    Console.WriteLine(temp5);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
+
