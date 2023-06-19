e178374a42b84b03ba371c18aa11c5024ed832bb
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-06-19 11:39:01 +0500

    C# Boxing and Unboxing
    https://10pearlsuniversity.org/courses/fundamentals-of-c/18504/

---
+++ TraineeSoftwareDeveloper/C#/13_BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿// BOXING VS UNBOXING // 
+
+// 1. Boxing
+int a = 10;
+object ao = a;
+Console.WriteLine($"Boxing:\n" +
+                  $"{a}\t{a.GetType()}\n" +
+                  $"{ao}\t{ao.GetType()}\n");
+// 2. UnBoxing
+object bo = 20;
+//int bTest = bo; // Error
+int b = (int)bo;
+Console.WriteLine($"Un-Boxing:\n" +
+                  $"{bo}\t{bo.GetType()}\n" +
+                  $"{b}\t{b.GetType()}\n");
+
+UnboxingClass ubo = new(30);
+//int ubTest = (int)ubo; //Error
+int ub = ubo.B;
+Console.WriteLine($"Un-Boxing:\n" +
+                  $"{ubo.B}\t{ubo.GetType()}\n" +
+                  $"{ub}\t{ub.GetType()}\n");
+
+class UnboxingClass
+{
+    public int B { get; set; }
+    public UnboxingClass(int b)
+    {
+        B = b;
+    }
+}
