13f9c0312da1b030a0222ac22932cd961bb6b813
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-06-20 11:51:19 +0500

    C# Array, List, ArrayList
    https://www.youtube.com/watch?v=BKynEBPqiIM&list=PLQMqUlV2_B8lC_MlZg-sMitFaA5-QJ0e4&index=1

---
+++ TraineeSoftwareDeveloper/C#/15_ArrayVsListVsArrayList/ArrayVsListVsArrayList/Program.cs
@@ -0,0 +1,23 @@
+﻿// ARRAY VS LIST VS ARRAYLIST //
+using System.Collections;
+
+// 1. Array (Fixed size, Strongly typed)
+int[] arr = new int[2];
+arr[0] = 1;
+arr[1] = 2;
+Array.Resize(ref arr, 3); // can do, but not preferred
+arr[2] = 3;
+// int[] arr2 = { 1, 2 };
+
+
+// 2. List (Flexible size, Strongly typed)
+List<int> list = new();
+list.Add(1);
+list.Add(2);
+
+
+// 3. ArrayList (Flexible size, Not strongly typed)
+ArrayList arrayList = new ArrayList();
+arrayList.Add(1);
+arrayList.Add(2);
+arrayList.Add("Saba"); // Boxing & UnBoxing
