92c15e4b5f21b18edb8ccdfd4fe841f82dd01801
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-03 11:59:08 +0500

    Clean-up - Remove extra files and code

--- TraineeSoftwareDeveloper/React/AmazonCloneApp/amazon-clone/src/App.js
+++ TraineeSoftwareDeveloper/React/AmazonCloneApp/amazon-clone/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    // BEM convention
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let's Build a Full-Stack AMAZON Clone with REACT JS (Full E-Commerce Store)</h1>
     </div>
   );
 }
