6328e0eb8291f2a25b44e37c7137989d82f225f9
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-04 16:48:21 +0500

    Header / Navbar - 55:42
    https://www.youtube.com/watch?v=RDV3Z1KCBvo

--- TraineeSoftwareDeveloper/React/AmazonCloneApp/amazon-clone/src/App.js
+++ TraineeSoftwareDeveloper/React/AmazonCloneApp/amazon-clone/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Header from './Header.js'
 
 function App() {
   return (
     //BEM Convention
     <div className="App">
+      {/* HEADER */}
+      <Header />
+
       <h1>Amazon Clone</h1>
-      <p>https://www.youtube.com/watch?v=RDV3Z1KCBvo</p>
+      <a href='https://www.youtube.com/watch?v=RDV3Z1KCBvo'>
+        <p>Let's Build a Full-Stack AMAZON Clone with REACT JS (Full E-Comm Store)</p>
+      </a>
+      
+      {/* HOME */}
     </div>
   );
 }
