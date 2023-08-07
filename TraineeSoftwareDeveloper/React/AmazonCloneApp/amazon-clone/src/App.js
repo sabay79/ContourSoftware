fe701bd28d6edef0a968cad493128b8adec300b8
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-07 09:53:45 +0500

    Home Component - Banner

--- TraineeSoftwareDeveloper/React/AmazonCloneApp/amazon-clone/src/App.js
+++ TraineeSoftwareDeveloper/React/AmazonCloneApp/amazon-clone/src/App.js
@@ -1,5 +1,7 @@
+import React from 'react'
 import './App.css';
 import Header from './Header.js'
+import Home from './Home.js'
 
 function App() {
   return (
@@ -7,13 +9,9 @@ function App() {
     <div className="App">
       {/* HEADER */}
       <Header />
-
-      <h1>Amazon Clone</h1>
-      <a href='https://www.youtube.com/watch?v=RDV3Z1KCBvo'>
-        <p>Let's Build a Full-Stack AMAZON Clone with REACT JS (Full E-Comm Store)</p>
-      </a>
       
       {/* HOME */}
+      <Home />
     </div>
   );
 }
---
+++ TraineeSoftwareDeveloper/React/AmazonCloneApp/amazon-clone/src/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+  return (
+    <div className='home'>
+        <div className='home__container'>
+            <img className='home__image' src='amazonBanner.jpg' alt='Banner' />
+
+            <div className='home__row'>
+                {/* Product */}
+                {/* Product */}
+            </div>
+            <div className='home__row'>
+                {/* Product */}
+                {/* Product */}
+                {/* Product */}
+            </div>
+            <div className='home__row'>
+                {/* Product */}
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Home
