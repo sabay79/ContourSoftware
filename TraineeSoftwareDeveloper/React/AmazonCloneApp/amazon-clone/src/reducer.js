ede212ded38f61accf9cf94ae2634be98fd45ec9
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-08 10:33:24 +0500

    Error Resolved 😃😅

--- TraineeSoftwareDeveloper/React/AmazonCloneApp/amazon-clone/src/reducer.js
+++ TraineeSoftwareDeveloper/React/AmazonCloneApp/amazon-clone/src/reducer.js
@@ -1,10 +1,11 @@
 export const initialState = { basket: [], };
 
 const reducer = (state, action) => {
-    switch(state.type)
+    switch(action.type)
     {
         case "ADD_TO_BASKET":
             return{ ...state, basket: [...state.basket, action.item], };
     }
 };
+
 export default reducer;
