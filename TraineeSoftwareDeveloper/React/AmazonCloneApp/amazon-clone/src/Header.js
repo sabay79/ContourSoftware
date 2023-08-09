dde6d89090433a6b5ae2964e7c306beaa51e0b50
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-09 15:56:02 +0500

    LogOut ➖🔓

--- TraineeSoftwareDeveloper/React/AmazonCloneApp/amazon-clone/src/Header.js
+++ TraineeSoftwareDeveloper/React/AmazonCloneApp/amazon-clone/src/Header.js
@@ -4,44 +4,52 @@ import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import {Link} from "react-router-dom";
 import {useStateValue} from './StateProvider';
+import {auth} from "./firebase.js";
 
 function Header() {
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket, user}, dispatch] = useStateValue();
+    const handleAuthentication = () => {
+        if(user){
+            auth.signOut();
+        }
+    }
 
-  return (
-    <div className='header'>
-        <Link to='/'>
-            <img className='header__logo' src='./amazonLogo.png' alt="logo" />
-        </Link>
-
-        <div className='header__search'>
-            <input className='header__searchInput' type='text' />
-            <SearchIcon className='header__searchIcon' />
-        </div>
-
-        <div className='header__nav'>
-            <Link to='/login'>
-                <div className='header__option'>
-                    <span className='header__optionLineOne'>Hello Guest</span>
-                    <span className='header__optionLineTwo'>Sign In</span>
-                </div>
+    return (
+        <div className='header'>
+            <Link to='/'>
+                <img className='header__logo' src='./amazonLogo.png' alt="logo" />
             </Link>
-            <div className='header__option'>
-                <span className='header__optionLineOne'>Returns</span>
-                <span className='header__optionLineTwo'>& Orders</span>
+
+            <div className='header__search'>
+                <input className='header__searchInput' type='text' />
+                <SearchIcon className='header__searchIcon' />
             </div>
-            <Link to='/checkout'>
-                <div className='header__optionBasket'>
-                    <ShoppingBasketIcon />
-                    <span className='header__optionLineTwo header__basketCount'>
-                        {basket.length}
-                    </span>
-                </div>
-            </Link>
-        </div>
 
-    </div>
-  )
+            <div className='header__nav'>
+                <Link to={!user && '/login'}>   {/* only directs to Login Page if there's no user */}
+                    <div onClick={handleAuthentication} className='header__option'>
+                        <span className='header__optionLineOne'>Hello Guest</span>
+                        <span className='header__optionLineTwo'>
+                            {user ? 'Sign Out' : 'Sign In'}
+                        </span>
+                    </div>
+                </Link>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Returns</span>
+                    <span className='header__optionLineTwo'>& Orders</span>
+                </div>
+                <Link to='/checkout'>
+                    <div className='header__optionBasket'>
+                        <ShoppingBasketIcon />
+                        <span className='header__optionLineTwo header__basketCount'>
+                            {basket.length}
+                        </span>
+                    </div>
+                </Link>
+            </div>
+
+        </div>
+    )
 }
 
 export default Header
