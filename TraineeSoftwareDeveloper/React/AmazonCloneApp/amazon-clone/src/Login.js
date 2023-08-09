cf4d021d5d036476640da774266b19e0e356ec30
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-09 10:38:44 +0500

    Login Form Basic UI

--- TraineeSoftwareDeveloper/React/AmazonCloneApp/amazon-clone/src/Login.js
+++ TraineeSoftwareDeveloper/React/AmazonCloneApp/amazon-clone/src/Login.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
 import './Login.css'
+import {Link} from "react-router-dom";
 
 function Login() {
   return (
-    <div>
-      <h1>Login Form</h1>
+    <div className='login'>
+
+      <Link to='/'>
+        <img className='login__logo' src='amazonLogoLightBD.png' alt='Amazon Logo' />
+      </Link>
+
+      <div className='login__container'>
+        <h1>SignIn</h1>
+        <form>
+          <h5>Email</h5>
+          <input type='email' />
+
+          <h5>Password</h5>
+          <input type='password' />
+
+          <button className='login__signInButton'>Sign In</button>
+        </form>
+
+        <p>
+        By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. 
+        Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+        </p>
+
+        <button className='login__registrationButton'>Create your Amazon Account</button>
+      </div>
     </div>
   )
 }
