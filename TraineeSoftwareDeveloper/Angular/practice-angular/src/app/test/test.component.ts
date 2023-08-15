b271d871fdce98fa174f7df4fb6b2678500b7b02
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-15 11:58:44 +0500

    ~~~ :ant:

--- TraineeSoftwareDeveloper/Angular/practice-angular/src/app/test/test.component.ts
+++ TraineeSoftwareDeveloper/Angular/practice-angular/src/app/test/test.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
+  template: `<div>Inline Template</div>`,
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
