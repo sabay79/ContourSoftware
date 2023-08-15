bcd4f922ad7529ca123fa4e080bacbc2efaa3b79
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-15 13:16:55 +0500

    Separate Header Component :airplane:

--- TraineeSoftwareDeveloper/Angular/practice-angular/src/app/app.module.ts
+++ TraineeSoftwareDeveloper/Angular/practice-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
