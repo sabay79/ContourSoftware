b48fc977d0be092a4a697036331ce92b0633cffa
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-15 12:10:14 +0500

    Inline HTML & CSS

--- TraineeSoftwareDeveloper/Angular/practice-angular/src/app/test/test.component.ts
+++ TraineeSoftwareDeveloper/Angular/practice-angular/src/app/test/test.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
+
   template: `<div>Inline Template</div>`,
   templateUrl: './test.component.html',
+
+  styles: [`div{ color: red; }`],
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
