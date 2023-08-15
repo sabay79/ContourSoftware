1aa65926b0bd78cb2195aefcd04136cae19ec26e
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-15 11:47:44 +0500

    Deleted 'temp' component (manually)

--- TraineeSoftwareDeveloper/Angular/practice-angular/src/app/app.module.ts
+++ TraineeSoftwareDeveloper/Angular/practice-angular/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
-import { TempComponent } from './temp/temp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    TempComponent
   ],
   imports: [
     BrowserModule
