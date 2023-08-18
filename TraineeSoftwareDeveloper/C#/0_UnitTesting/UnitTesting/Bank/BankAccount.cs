0b1a3f2256322630b479ee3466077b7b442e0e68
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-18 11:45:16 +0500

    C# Unit Testing -  Refactor the code under test
    https://learn.microsoft.com/en-us/visualstudio/test/walkthrough-creating-and-running-unit-tests-for-managed-code?view=vs-2022#refactor-the-code-under-test

--- TraineeSoftwareDeveloper/C#/0_UnitTesting/UnitTesting/Bank/BankAccount.cs
+++ TraineeSoftwareDeveloper/C#/0_UnitTesting/UnitTesting/Bank/BankAccount.cs
@@ -6,6 +6,9 @@ public class BankAccount
     private readonly string m_customerName;
     private double m_balance;
 
+    public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+    public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
+
     private BankAccount() { }
 
     public BankAccount(string customerName, double balance)
@@ -28,12 +31,12 @@ public class BankAccount
     {
         if (amount > m_balance)
         {
-            throw new ArgumentOutOfRangeException("amount");
+            throw new ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
         }
 
         if (amount < 0)
         {
-            throw new ArgumentOutOfRangeException("amount");
+            throw new ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
         }
 
         //m_balance -= amount; // intentionally incorrect code
@@ -52,10 +55,11 @@ public class BankAccount
 
     public static void Main()
     {
-        BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+        BankAccount ba = new("Mr. Bryan Walton", 11.99);
 
         ba.Credit(5.77);
         ba.Debit(11.22);
+
         Console.WriteLine("Current balance is ${0}", ba.Balance);
     }
 }
