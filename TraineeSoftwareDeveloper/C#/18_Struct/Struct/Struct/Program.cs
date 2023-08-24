214604b7bd76e46fb2fbff47cd97219f1b77a113
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-24 14:44:51 +0500

    C# Struct

---
+++ TraineeSoftwareDeveloper/C#/18_Struct/Struct/Struct/Program.cs
@@ -0,0 +1,23 @@
+﻿// STRUCT //
+Console.WriteLine("Struct");
+
+//Coords coords;
+//coords.X = 10.20;
+//coords.Y = 20.30;
+
+Coords coords = new(10.20, 20.30);
+Console.WriteLine(coords.SumOfCoords());
+
+struct Coords
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Coords(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public readonly double SumOfCoords() => X + Y;
+}
