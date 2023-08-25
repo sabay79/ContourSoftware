11950d1d967152115bfff1948112b8171543817c
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-25 18:14:07 +0500

    Repository - Interface & Service - Consume DbContext Class

---
+++ TraineeSoftwareDeveloper/Angular/PaymentCRUDwithWebAPI/Repository/Payment.Data.Interfaces/IPaymentDetailRepository.cs
@@ -0,0 +1,13 @@
+﻿using Payment.Data.Models;
+
+namespace Payment.Data.Interfaces
+{
+    public interface IPaymentDetailRepository
+    {
+        IEnumerable<PaymentDetail> GetAll();
+        PaymentDetail GetById(int id);
+        void Add(PaymentDetail paymentDetail);
+        void Update(PaymentDetail paymentDetail);
+        void Delete(PaymentDetail paymentDetail);
+    }
+}
---
+++ TraineeSoftwareDeveloper/Angular/PaymentCRUDwithWebAPI/Repository/Payment.Data.Services/PaymentDetailRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Payment.Data.Interfaces;
+using Payment.Data.Models;
+
+namespace Payment.Data.Services
+{
+    public class PaymentDetailRepository : IPaymentDetailRepository
+    {
+        private readonly PaymentDetailContext _dbContext;
+        private readonly DbSet<PaymentDetail> _dbSet;
+
+        public PaymentDetailRepository(PaymentDetailContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<PaymentDetail>();
+        }
+        public IEnumerable<PaymentDetail> GetAll() => _dbSet.ToList();
+
+        public PaymentDetail GetById(int id) => _dbSet.Find(id);
+
+        public void Add(PaymentDetail paymentDetail)
+        {
+            _dbSet.Add(paymentDetail);
+            Save();
+        }
+
+        public void Update(PaymentDetail paymentDetail)
+        {
+            _dbSet.Update(paymentDetail);
+            Save();
+        }
+
+        public void Delete(PaymentDetail paymentDetail)
+        {
+            _dbSet.Remove(paymentDetail);
+            Save();
+        }
+
+        private void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
