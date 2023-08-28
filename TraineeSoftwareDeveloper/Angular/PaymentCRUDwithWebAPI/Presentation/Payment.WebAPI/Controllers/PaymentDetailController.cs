7f889aa6fef50e4b4085e995be29570d880fdd9a
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-28 12:45:05 +0500

    Added API Controller (Working) + Consume Repository in it bcz Business Layer not working because of AutoMapper

---
+++ TraineeSoftwareDeveloper/Angular/PaymentCRUDwithWebAPI/Presentation/Payment.WebAPI/Controllers/PaymentDetailController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Payment.Business.Models;
+using Payment.Data.Interfaces;
+using Payment.Data.Models;
+
+namespace Payment.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailController : ControllerBase
+    {
+        private readonly IPaymentDetailRepository _paymentDetailService;
+
+        public PaymentDetailController(IPaymentDetailRepository paymentDetailService)
+        {
+            _paymentDetailService = paymentDetailService;
+        }
+
+        // GET: api/<PaymentDetailController>
+        [HttpGet]
+        public ActionResult<IEnumerable<PaymentDetailModel>> Get() => Ok(_paymentDetailService.GetAll());
+
+        // GET api/<PaymentDetailController>/5
+        [HttpGet("{id}")]
+        public ActionResult<PaymentDetailModel> Get(int id) => Ok(_paymentDetailService.GetById(id));
+
+        // POST api/<PaymentDetailController>
+        [HttpPost]
+        public ActionResult<PaymentDetailModel> Post([FromBody] PaymentDetail paymentDetailModel)
+        {
+            _paymentDetailService.Add(paymentDetailModel);
+            return CreatedAtAction(nameof(Get), new { id = paymentDetailModel.PaymentDetailID }, paymentDetailModel);
+        }
+
+        // PUT api/<PaymentDetailController>/5
+        [HttpPut("{id}")]
+        public ActionResult<PaymentDetailModel> Put([FromBody] PaymentDetail paymentDetailModel)
+        {
+            _paymentDetailService.Update(paymentDetailModel);
+            return Ok(paymentDetailModel);
+        }
+
+        // DELETE api/<PaymentDetailController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var paymentDetail = _paymentDetailService.GetById(id);
+            _paymentDetailService.Delete(paymentDetail);
+            return Ok(_paymentDetailService.GetById(id));
+        }
+    }
+}
