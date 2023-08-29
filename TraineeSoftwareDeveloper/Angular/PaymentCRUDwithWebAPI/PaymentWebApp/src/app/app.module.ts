3ead568e9cb2fc953fbee320ccf94d4f35d32caa
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-29 11:21:05 +0500

    Create Payment Detail Component

--- TraineeSoftwareDeveloper/Angular/PaymentCRUDwithWebAPI/PaymentWebApp/src/app/app.module.ts
+++ TraineeSoftwareDeveloper/Angular/PaymentCRUDwithWebAPI/PaymentWebApp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent
   ],
   imports: [
     BrowserModule
