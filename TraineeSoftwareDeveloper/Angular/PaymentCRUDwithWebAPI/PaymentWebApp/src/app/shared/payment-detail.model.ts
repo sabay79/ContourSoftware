b233f61b1f8204d832bb73250b3d7b1c37ce487b
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-08-29 12:21:49 +0500

    Create a Model in Angular Corresponding to Model in C#

--- TraineeSoftwareDeveloper/Angular/PaymentCRUDwithWebAPI/PaymentWebApp/src/app/shared/payment-detail.model.ts
+++ TraineeSoftwareDeveloper/Angular/PaymentCRUDwithWebAPI/PaymentWebApp/src/app/shared/payment-detail.model.ts
@@ -1,2 +1,8 @@
-export class PaymentDetail {
+export class PaymentDetail 
+{
+    paymentDetailID: number = 0
+    cardOwnerName: string = ""
+    cardNumber: string = ""
+    expirationDate: string = ""
+    securityCode: string = ""
 }
