d494dd1cee93e0e143af5d2adb2193385a99ab11
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-09-12 16:05:41 +0500

    Clean Up :|

--- TraineeSoftwareDeveloper/VueJS/ToDoApp/ToDoAppWebAPI/ToDoApp.WebAPI/Model/NoteBusinessServices.cs
+++ TraineeSoftwareDeveloper/VueJS/ToDoApp/ToDoAppWebAPI/ToDoApp.WebAPI/Model/NoteBusinessServices.cs
@@ -16,20 +16,6 @@ namespace ToDoApp.WebAPI.Model
 
         public Note Get(int id) => _dbContext.Notes.Find(id);
 
-        //public async void Add(Note note)
-        //{
-        //    //await _dbContext.Notes.AddAsync(note);
-        //    _dbContext.Entry(note).State = EntityState.Added;
-        //    await _dbContext.SaveChangesAsync();
-        //}
-
-        //public async void Update(Note note)
-        //{
-        //    //_dbContext.Notes.Update(note);
-        //    _dbContext.Entry(note).State = EntityState.Modified;
-        //    await _dbContext.SaveChangesAsync();
-        //}
-
         /// <summary>
         /// Entity states and SaveChanges
         /// https://learn.microsoft.com/en-us/ef/ef6/saving/change-tracking/entity-state?redirectedfrom=MSDN
