31737c061354055c284d2a016d51124b573390ad
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-09-13 12:28:09 +0500

    Some More work

---
+++ TraineeSoftwareDeveloper/VueJS/ToDoApp/ToDoWebApp/todo-app/src/components/NoteForm.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="note-form">
+        <form @submit.prevent="handleSubmit">
+            <label for="Description">Note</label>
+            <input v-model="note.description" type="text" id="Description" />
+
+            <button>Add Note</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'note-form',
+        data()
+        {
+            return{
+                note : { description : '', },
+            }
+        },
+        methods:{
+            handleSubmit(){
+                this.$emit('add:note', this.note);
+                console.log('testing handleSubmit');
+            },
+        },
+    }
+</script>
+
+<style scoped>
+    form{
+        margin-bottom: 2rem;
+    }
+</style>
