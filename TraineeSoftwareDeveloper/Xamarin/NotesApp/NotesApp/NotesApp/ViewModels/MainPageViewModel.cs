16420fd6818566801b75b8be050c6312ebe68aa7
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-09-20 13:13:58 +0500

    Xamarin.Forms MVVM with XAML | Xamarin 101 [6 of 11]
    https://www.youtube.com/watch?v=VqZeTAjsgFQ&list=PLdo4fOcmZ0oU10SXt2W58pu2L0v2dOW-1&index=6

---
+++ TraineeSoftwareDeveloper/Xamarin/NotesApp/NotesApp/NotesApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace NotesApp.ViewModels
+{
+    class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            AllNotes = new ObservableCollection<string>();
+
+            SaveCommand = new Command(() =>
+            {
+                AllNotes.Add(TheNote);
+                TheNote = string.Empty;
+            });
+
+            EraseCommand = new Command(() =>
+            {
+                TheNote = string.Empty;
+            });
+        }
+
+        public ObservableCollection<string> AllNotes { get; set; }
+    
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string theNote;
+        public string TheNote
+        {
+            get => theNote;
+            set
+            {
+                theNote = value;
+                var args = new PropertyChangedEventArgs(nameof(TheNote)); // Let the View know that the property has changed...
+                PropertyChanged?.Invoke(this, args); // Let the ViewModel know that the property has changed...
+            }
+        }
+
+        public Command SaveCommand { get; set; }
+        public Command EraseCommand { get; }
+    }
+}
