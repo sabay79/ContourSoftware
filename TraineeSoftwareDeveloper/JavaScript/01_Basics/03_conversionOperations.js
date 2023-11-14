feb696fdc1b15af6fa1dd3bf0649af10ae222b51
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-11-14 13:58:30 +0500

    JS Operators
    https://www.youtube.com/watch?v=N9el4APFtAo&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=7

--- TraineeSoftwareDeveloper/JavaScript/01_Basics/03_conversionOperations.js
+++ TraineeSoftwareDeveloper/JavaScript/01_Basics/03_conversionOperations.js
@@ -18,3 +18,45 @@ console.log(typeof booleanIsLoggedIn);
 // 1 => true; 0 => false
 // "" => false
 // "Saba" => true
+
+// *********************** Operations ***********************
+
+let value = 7;
+let negValue = -value;
+console.log(negValue);
+
+console.log(3+2);
+console.log(3-2);
+console.log(3*2);
+console.log(3**2);
+console.log(3/2);
+console.log(3%2);
+
+let str1 = "Hello"
+let str2 = "Saba"
+let str3 = str1 + ', ' + str2
+console.log(str3);
+
+// MOT RECOMMENDED //
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+// MOT RECOMMENDED //
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
