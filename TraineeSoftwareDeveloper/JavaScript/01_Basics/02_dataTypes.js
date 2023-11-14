d566067bdd584d407f899f8ea05a0ccb9e874844
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-11-14 12:29:39 +0500

    JS Data Types - Primitive
    https://www.youtube.com/watch?v=-9knnv97wSc&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=6

---
+++ TraineeSoftwareDeveloper/JavaScript/01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // Treat all JS code as newer version 
+
+//alert(3+3) // we're using NodeJs, not Browser
+
+let name = 'Saba' // string
+let age = 23 // number
+let isLearning = true // boolean
+let temp = null; // null
+let state; // undefined
+
+// Primitive Data Types //
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof(name));
+console.log(typeof name);
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
