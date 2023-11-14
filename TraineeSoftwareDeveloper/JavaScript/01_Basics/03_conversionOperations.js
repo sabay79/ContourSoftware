ef65bb2efef74c286de76719d8c8904dbaf2ff3c
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-11-14 13:23:37 +0500

    JS Conversion Operations
    https://www.youtube.com/watch?v=X7hDBhd_L5U&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=6

---
+++ TraineeSoftwareDeveloper/JavaScript/01_Basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score = "33";
+console.log(typeof(score));
+
+let scoreInNumber = Number(score);
+console.log(typeof(scoreInNumber));
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 0;
+console.log(typeof isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Saba" => true
+
