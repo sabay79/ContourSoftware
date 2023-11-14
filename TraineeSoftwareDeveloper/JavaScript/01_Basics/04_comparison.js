c5cc8bc1c3d0b8d52637f1664a1c0a6b9d0c1d9a
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-11-14 14:39:01 +0500

    JS Comparison
    https://www.youtube.com/watch?v=giP2uXMlv4c&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=8

---
+++ TraineeSoftwareDeveloper/JavaScript/01_Basics/04_comparison.js
@@ -0,0 +1,28 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// Avoid these Comparisons //
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// False in all cases
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// Avoid these Comparisons //
+
+
+// === 
+// Strict Check
+
+console.log("2" === 2);
