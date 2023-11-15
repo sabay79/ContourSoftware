8fa5a7dbe0c750280bb375a3e3ae05cc6c68e79f
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-11-15 16:33:03 +0500

    JS Variable's Scope
    https://www.youtube.com/watch?v=cHHU0jXfjKY&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=21

---
+++ TraineeSoftwareDeveloper/JavaScript/03_Basics/02_scopes.js
@@ -0,0 +1,17 @@
+let a = 10
+const b = 20
+var c = 30 
+
+if(true) {
+    let a = 100
+    const b = 200
+    var c = 300
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
