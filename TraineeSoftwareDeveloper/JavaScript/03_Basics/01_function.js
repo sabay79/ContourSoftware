5e40ee2cfb831775295107d8b3aafb9ba9ffaa92
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-11-15 16:13:20 +0500

    JS Function
    https://www.youtube.com/watch?v=Bn56WahG_t0&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=19&pp=iAQB
    
    https://www.youtube.com/watch?v=t7ZHPhgdA4U&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=21

---
+++ TraineeSoftwareDeveloper/JavaScript/03_Basics/01_function.js
@@ -0,0 +1,15 @@
+function myName() {
+    console.log("Saba");
+}
+
+function addTwoNumbers(num1, num2) {
+    //console.log(num1 + num2);
+    return num1 + num2;
+}
+
+myName();
+console.log(addTwoNumbers(10, 20));
+ 
+// function with object as param
+
+// function with array as param
