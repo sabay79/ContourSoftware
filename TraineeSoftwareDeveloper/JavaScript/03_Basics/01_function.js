fc77e06efd99905ed982d43df03b62d4cd1dcf7e
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-11-15 18:06:30 +0500

    JS Arrow Function & 'this' keyword & Rest Operator
    https://www.youtube.com/watch?v=9ksqBa8_txM&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=23
    
    https://www.youtube.com/watch?v=t7ZHPhgdA4U&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=20

--- TraineeSoftwareDeveloper/JavaScript/03_Basics/01_function.js
+++ TraineeSoftwareDeveloper/JavaScript/03_Basics/01_function.js
@@ -13,3 +13,57 @@ console.log(addTwoNumbers(10, 20));
 // function with object as param
 
 // function with array as param
+
+////////////////////////////// this Operator //////////////////////////////
+
+const user = {
+    username: 'saba',
+    email: 'saba@gmail.com',
+
+    welcomeMessage: function() {
+        console.log(`${this.username}, Welcome to Website`);
+
+        console.log(this);
+        console.log(typeof this);
+    }
+}
+user.welcomeMessage();
+
+function func1(){
+    let username = 'func 1';
+    console.log(this);
+    console.log(username);
+    console.log(this.username);
+}
+func1();
+
+const func2 = function() {
+    let username = 'func 2';
+    console.log(this);
+    console.log(username);
+    console.log(this.username);
+}
+func2();
+
+function sumRestOperator(n1, n2, ...n) {
+    return [n1+n2, n];
+}
+console.log(sumRestOperator(1, 2, 1, 1, 1, 1, 1));
+
+///////////////////////////// Arrow Function /////////////////////////////
+
+const func3 = () => {
+    let username = 'func 3';
+    console.log(this);
+    console.log(username);
+    console.log(this.username);
+}
+func3();
+
+const sum = (num1, num2) => {
+    return num1 + num2;
+}
+console.log(sum(10, 20));
+
+const sumImplicit = (num1, num2) => num1 + num2;
+console.log(sumImplicit(10, 20));
