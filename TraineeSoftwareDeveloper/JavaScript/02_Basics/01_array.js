ddf85c7f43c47b4e7072227c195f2e6132caed22
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-11-15 12:31:37 +0500

    JS Array Cont.
    https://www.youtube.com/watch?v=m6azhgyCi-k&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=15&pp=iAQB

--- TraineeSoftwareDeveloper/JavaScript/02_Basics/01_array.js
+++ TraineeSoftwareDeveloper/JavaScript/02_Basics/01_array.js
@@ -36,3 +36,32 @@ console.log("B", arr1);
 const spliceArr = arr1.splice(1, 3);
 console.log(spliceArr, "splice");
 console.log("C", arr1);
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+marvel_heros.push(dc_heros)
+console.log(marvel_heros);
+
+// CONCAT vs SPREAD //
+
+const allHeros_Concat = marvel_heros.concat(dc_heros)
+console.log(allHeros_Concat);
+
+const allHeros_Spread = [...marvel_heros, ...dc_heros]
+console.log(allHeros_Spread);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("Saba"))
+console.log(Array.from("Saba"))
+console.log(Array.from({name: "Saba"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
