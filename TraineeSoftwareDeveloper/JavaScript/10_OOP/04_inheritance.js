0b5a8ccdcd84bbcb3405186c5e3da36a60a5b2cb
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-11-24 17:27:37 +0500

    JS Inheritance via Class
    https://www.youtube.com/watch?v=u6mVHkMpoMk&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=45

--- TraineeSoftwareDeveloper/JavaScript/10_OOP/04_inheritance.js
+++ TraineeSoftwareDeveloper/JavaScript/10_OOP/04_inheritance.js
@@ -21,3 +21,38 @@ Teacher.__proto__ = User;
 
 // Modern Syntax
 Object.setPrototypeOf(TeachingSupport, Teacher);
+
+//////////////////////////////////////////////////////////////////////
+
+class UserClass{
+    constructor(username){
+        this.username = username;
+    }
+
+    logMe(){
+        console.log(`Username is ${this.username}`);
+    }
+}
+
+class TeacherClass extends UserClass{
+    constructor(username, email, password){
+        super(username);
+        this.email = email;
+        this.password = password;
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+const user = new UserClass('saba');
+user.logMe();
+
+const teacher = new TeacherClass('ayesha', 'ayesha@gmail.com', 12345);
+teacher.logMe();
+
+console.log(user instanceof UserClass);
+console.log(user instanceof TeacherClass);
+
+console.log(teacher instanceof UserClass);
+console.log(teacher instanceof TeacherClass);
