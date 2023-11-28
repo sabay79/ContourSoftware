903c3aab61a712e46ad6b6bb70de12ded562ab27
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-11-28 15:55:10 +0500

    React useState usecases related to increment
    https://www.youtube.com/watch?v=tOYkV6Yhrhs&list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige&index=8

--- TraineeSoftwareDeveloper/React/React-Tutorial-Series/02-counter/src/App.jsx
+++ TraineeSoftwareDeveloper/React/React-Tutorial-Series/02-counter/src/App.jsx
@@ -2,11 +2,36 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  let [count, setCount] = useState(15);
+  let [count, setCount] = useState(3);
 
   const addValue = () => {
     if(count <= 7){
+      setCount(count + 1);
+
+      //#region Only increment by 1 
+      // setCount(count + 1);
+      // setCount(count + 1);
+      // setCount(count + 1);
+      //#endregion
+
+      //#region increment by no of calls(3)
+      // setCount(prevCount => prevCount + 1)
+      // setCount(prevCount => prevCount + 1)
+      // setCount(prevCount => prevCount + 1)
+      //#endregion
+
+      //#region increment by no of calls(3)
       setCount(count++);
+      setCount(count++);
+      setCount(count++);
+      //#endregion
+
+      //#region increment by no of calls-1(2)
+      // setCount(++count);
+      // setCount(++count);
+      // setCount(++count);
+      //#endregion
+
     } else {
       alert('Reached Maximum Limit');
     }  
