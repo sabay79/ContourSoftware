88d71cb334a2b01db604f178994d98208e42c79e
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-12-01 12:54:29 +0500

    React Context API - Approach 02
    https://www.youtube.com/watch?v=JQVBGtZMqgU&list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige&index=17
    
    After 38:00

---
+++ TraineeSoftwareDeveloper/React/React-Tutorial-Series/09-theme-switcher/src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { ThemeProvider } from './context/Theme';
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
+
+function App() {
+
+  const [themeMode, setThemeMode] = useState('light');
+
+  const lightTheme = () => {
+    setThemeMode('light');
+  }
+
+  const darkTheme = () => {
+    setThemeMode('dark');
+  }
+
+  // Actual Change in a Theme
+  useEffect(() => {
+
+    const htmlClassList = document.querySelector('html').classList;
+    
+    htmlClassList.remove('light', 'dark');
+    htmlClassList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              {/* ThemeBtn */}
+              <ThemeBtn/>
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+              {/* Card */}
+              <Card/>
+            </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ TraineeSoftwareDeveloper/React/React-Tutorial-Series/09-theme-switcher/src/context/Theme.js
@@ -0,0 +1,15 @@
+// Better Approach //
+
+import { createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode: 'light',
+    lightTheme: () => {},
+    darkTheme: () => {},
+});
+
+export const ThemeProvider = ThemeContext.Provider;
+
+export default function useTheme() {
+    return useContext(ThemeContext);
+}
