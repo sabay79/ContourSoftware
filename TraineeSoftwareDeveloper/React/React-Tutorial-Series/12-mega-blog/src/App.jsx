df4d136dee670bbcbbe36a8523e05a43dfa14a42
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-12-04 15:05:00 +0500

    React Blog Project - ENV and Appwrite  in React App
    https://www.youtube.com/watch?v=4_JlIr8yry0&list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige&index=19

---
+++ TraineeSoftwareDeveloper/React/React-Tutorial-Series/12-mega-blog/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+    <a href="https://www.youtube.com/watch?v=4_JlIr8yry0&list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige&index=19">
+      <h1>ENV and Appwrite in React Project</h1>
+    </a>
+    </>
+  );
+}
+
+export default App;
