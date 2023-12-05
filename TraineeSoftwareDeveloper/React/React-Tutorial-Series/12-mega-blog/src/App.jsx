c25ccf73968efa0b8a30e6a0738a02fc486e038b
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-12-05 12:23:44 +0500

    React Blog Project - How to Configure Redux Toolkit
    https://www.youtube.com/watch?v=8QGKg_W5sDQ&list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige&index=22

--- TraineeSoftwareDeveloper/React/React-Tutorial-Series/12-mega-blog/src/App.jsx
+++ TraineeSoftwareDeveloper/React/React-Tutorial-Series/12-mega-blog/src/App.jsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from 'react';
+import {useDispatch} from 'react-redux';
 import './App.css';
+
+import authService from './appwrite/auth';
+import {login, logout} from './store/authSlice';
+
+import {Header, Footer} from './components';
 
 function App() {
 
-  return (
-    <>
-    <a href="https://www.youtube.com/watch?v=0Py5cGGW2lE&list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige&index=20">
-      <h1>Build Authentication Service with Appwrite</h1>
-    </a>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if(userData){
+          dispatch(login({userData}));
+        } else{
+          dispatch(logout());
+        }
+      })
+      .catch((error) => alert(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between'>
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
