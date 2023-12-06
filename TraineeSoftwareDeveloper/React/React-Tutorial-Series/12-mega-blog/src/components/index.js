2fc4d4ae987e3470227539b1ad5ce1ab76f234a0
Saba Yashfeen <63623375+sabay79@users.noreply.github.com>
2023-12-06 16:00:30 +0500

    React Blog Project - Adding Form and Slug Values
    https://www.youtube.com/watch?v=-6LvNku2nJE&list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige&index=25

--- TraineeSoftwareDeveloper/React/React-Tutorial-Series/12-mega-blog/src/components/index.js
+++ TraineeSoftwareDeveloper/React/React-Tutorial-Series/12-mega-blog/src/components/index.js
@@ -4,10 +4,30 @@ import LogoutBtn from './Header/LogoutBtn';
 import Container from "./container/Container";
 import Logo from "./Logo/Logo";
 
+import Button from './Button';
+import Input from './Input';
+import Select from './Select';
+
+import Login from './Login';
+import Signup from './Signup';
+
+import AuthLayout from './AuthLayout';
+import PostCard from './PostCard';
+
+import RTE from './RTE';
+
 export {
     Header,
     Footer,
     LogoutBtn,
     Container,
-    Logo,    
+    Logo,  
+    Button,
+    Input,
+    Select,
+    Login,
+    Signup,
+    AuthLayout,
+    PostCard,
+    RTE,
 };
